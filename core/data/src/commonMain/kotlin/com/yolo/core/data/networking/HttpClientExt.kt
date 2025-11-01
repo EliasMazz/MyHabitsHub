fb47882e2c35b89c2144ab8a9966eeecac5a5877
eliasmazz <elias.mazzocco@gmail.com>
2025-11-01 14:38:33 -0300

    feat: Add HTTP client extension functions for common operations
    Introduce `post`, `get`, `put`, and `delete` extension functions for Ktor `HttpClient` to standardize and simplify networking patterns with route, query parameters, and request body handling.

--- core/data/src/commonMain/kotlin/com/yolo/core/data/networking/HttpClientExt.kt
+++ core/data/src/commonMain/kotlin/com/yolo/core/data/networking/HttpClientExt.kt
@@ -2,14 +2,92 @@ package com.yolo.core.data.networking
 
 import com.yolo.core.domain.util.DataError
 import com.yolo.core.domain.util.ResultData
+import io.ktor.client.HttpClient
 import io.ktor.client.call.NoTransformationFoundException
 import io.ktor.client.call.body
+import io.ktor.client.request.HttpRequestBuilder
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 
 expect suspend fun <T> platformSafeCall(
     execute: suspend () -> HttpResponse,
     handleResponse: suspend (HttpResponse) -> ResultData<T, DataError.Remote>
 ): ResultData<T, DataError.Remote>
+
+suspend inline fun <reified Request, reified Response : Any> HttpClient.post(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): ResultData<Response, DataError.Remote> {
+    return safeCall {
+        post {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Response : Any> HttpClient.get(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): ResultData<Response, DataError.Remote> {
+    return safeCall {
+        get {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Response : Any> HttpClient.delete(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): ResultData<Response, DataError.Remote> {
+    return safeCall {
+        delete {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            builder()
+        }
+    }
+}
+
+suspend inline fun <reified Request, reified Response : Any> HttpClient.put(
+    route: String,
+    queryParams: Map<String, Any> = mapOf(),
+    body: Request,
+    crossinline builder: HttpRequestBuilder.() -> Unit = {}
+): ResultData<Response, DataError.Remote> {
+    return safeCall {
+        put {
+            url(constructRoute(route))
+            queryParams.forEach { (key, value) ->
+                parameter(key, value)
+            }
+            setBody(body)
+            builder()
+        }
+    }
+}
+
 
 suspend inline fun <reified T> safeCall(
     noinline execute: suspend () -> HttpResponse
@@ -22,14 +100,15 @@ suspend inline fun <reified T> safeCall(
 }
 
 suspend inline fun <reified T> responseToResult(response: HttpResponse): ResultData<T, DataError.Remote> {
-    return when(response.status.value) {
+    return when (response.status.value) {
         in 200..299 -> {
             try {
                 ResultData.Success(response.body<T>())
-            } catch(e: NoTransformationFoundException) {
+            } catch (e: NoTransformationFoundException) {
                 ResultData.Failure(DataError.Remote.SERIALIZATION)
             }
         }
+
         400 -> ResultData.Failure(DataError.Remote.BAD_REQUEST)
         401 -> ResultData.Failure(DataError.Remote.UNAUTHORIZED)
         403 -> ResultData.Failure(DataError.Remote.FORBIDDEN)
