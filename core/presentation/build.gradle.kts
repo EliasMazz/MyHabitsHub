49d0d3bd2851893251aba6d603d1f1d7910bea78
eliasmazz <elias.mazzocco@gmail.com>
2025-11-01 22:02:45 -0300

    refactor: Remove platform-specific functions and add adaptive device configuration utility
    - Introduced `DeviceConfiguration` utility to determine device type and orientation using adaptive APIs.
    - Updated `core/presentation` to include new dependencies for material3 adaptive UI components.

--- core/presentation/build.gradle.kts
+++ core/presentation/build.gradle.kts
@@ -8,6 +8,7 @@ kotlin {
         commonMain {
             dependencies {
                 implementation(libs.kotlin.stdlib)
+                implementation(libs.material3.adaptive)
                 implementation(projects.core.domain)
                 implementation(compose.components.resources)
             }
