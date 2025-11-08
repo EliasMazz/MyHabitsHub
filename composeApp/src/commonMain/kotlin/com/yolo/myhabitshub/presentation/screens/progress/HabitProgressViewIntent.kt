9d91c162efbde1e3f1676f1cdf66ae3b87b30025
eliasmazz <elias.mazzocco@gmail.com>
2025-11-08 09:26:34 -0300

    refactor: Simplify imports for ViewEvent and ViewIntent

--- composeApp/src/commonMain/kotlin/com/yolo/myhabitshub/presentation/screens/progress/HabitProgressViewIntent.kt
+++ composeApp/src/commonMain/kotlin/com/yolo/myhabitshub/presentation/screens/progress/HabitProgressViewIntent.kt
@@ -1,5 +1,5 @@
 package com.yolo.myhabitshub.presentation.screens.progress
 
-import com.yolo.myhabitshub.core.presentation.viewmodel.BaseViewModel
+import com.yolo.myhabitshub.core.presentation.viewmodel.BaseViewModel.ViewIntent
 
-sealed interface HabitProgressViewIntent: BaseViewModel.ViewIntent
+sealed interface HabitProgressViewIntent: ViewIntent
